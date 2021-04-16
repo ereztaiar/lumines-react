a5097c0e4edae917af2f201792379da437bbf9b4
Erez Taiar <taiar@techmarketing.co.il>
2021-04-16 11:50:35 +0300

    main: removed flare

--- src/components/Charecter/Charecter.jsx
+++ src/components/Charecter/Charecter.jsx
@@ -1,11 +1,9 @@
 import React from 'react';
-import flare from "../../assets/images/flare.png";
 import {FaRegGrinStars} from "react-icons/fa";
 
 const Character = () =>{
     return (
         <div className={"character"}>
-            <img src={flare} alt={"flare"}/>
             <FaRegGrinStars/>
         </div>
     )
