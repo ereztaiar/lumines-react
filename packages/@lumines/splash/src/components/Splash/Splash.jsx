e3d2db9397737d8b2eb46cf65f04d6eeb8d442be
erez taiar <ereztaiar@gmail.com>
2026-05-13 15:09:12 +0300

    refactor code intentations

--- packages/@lumines/splash/src/components/Splash/Splash.jsx
+++ packages/@lumines/splash/src/components/Splash/Splash.jsx
@@ -1,63 +1,57 @@
-import React, {  useState } from "react";
+import React, { useState } from "react";
 import { FaReact } from "react-icons/fa";
-import { default as splashStyle } from '@lumines/splash/src/styles/splash.less';
+import { default as splashStyle } from "@lumines/splash/src/styles/splash.less";
 import useTimer from "@lumines/core/src/hooks/useTimer";
-
-
 
 const COLORS_SWATCH = 5;
 
-const Block = props => {
-    const {
-        classes
-    } = props;
+const Block = (props) => {
+  const { classes } = props;
 
-    return (
-        <>
-            <div className={classes}></div>
-        </>
-    );
+  return <div className={classes}></div>;
 };
 
-const Splash = props => {
-
-    const getBlocks = () => {
-        const blocks = (
-            <>
-                {[...Array(8 * 8)].map((item, idx) => {
-
-                    const blockIdx = Math.floor(Math.random() * COLORS_SWATCH + 1);
-                    const blockClass = splashStyle[`block-${blockIdx}`];
-                    return <Block key={idx} classes={`${splashStyle.block} ${blockClass}`} />
-                })}
-            </>
-        );
-        return blocks;
-    }
-
-    const [blocks, setBlocks] = useState(getBlocks);
-
-    
-
-
-    useTimer(() => {
-        setBlocks(getBlocks());
-    }, 2500);
-
-
-    return (
-        <div className={splashStyle.root}>
-            {blocks}
-            <div className={splashStyle.titleCard}>
-                <h3>PUZZLE <FaReact /> FUSION</h3>
-                <h1>LUMINES REACT AI REMASTER</h1>
-                <h2>PUZZLE<img src={"/favicon.svg"} />FUSION</h2>
-            </div>
-            <div className={splashStyle.action}>
-                <h4>PRESS SPACE KEY</h4>
-            </div>
-        </div>
+const Splash = (props) => {
+  const getBlocks = () => {
+    const blocks = (
+      <>
+        {[...Array(8 * 8)].map((item, idx) => {
+          const blockIdx = Math.floor(Math.random() * COLORS_SWATCH + 1);
+          const blockClass = splashStyle[`block-${blockIdx}`];
+          return (
+            <Block key={idx} classes={`${splashStyle.block} ${blockClass}`} />
+          );
+        })}
+      </>
     );
+    return blocks;
+  };
+
+  const [blocks, setBlocks] = useState(getBlocks);
+
+  useTimer(() => {
+    setBlocks(getBlocks());
+  }, 2500);
+
+  return (
+    <div className={splashStyle.root}>
+      {blocks}
+      <div className={splashStyle.titleCard}>
+        <h3>
+          PUZZLE <FaReact /> FUSION
+        </h3>
+        <h1>LUMINES REACT AI REMASTER</h1>
+        <h2>
+          PUZZLE
+          <img src={"/favicon.svg"} />
+          FUSION
+        </h2>
+      </div>
+      <div className={splashStyle.action}>
+        <h4>PRESS SPACE KEY</h4>
+      </div>
+    </div>
+  );
 };
 
 export default Splash;
