2b79e137d04d1d72f2a36870c549ecbe4c29943a
erez taiar <ereztaiar@gmail.com>
2026-05-14 18:09:40 +0300

    updated splash screen style

--- packages/@lumines/splash/src/components/Splash/Splash.jsx
+++ packages/@lumines/splash/src/components/Splash/Splash.jsx
@@ -1,9 +1,14 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { FaReact } from "react-icons/fa";
-import { default as splashStyle } from "@lumines/splash/src/styles/splash_2.less";
+import { default as splashDefault } from "@lumines/splash/src/styles/default.less";
+import { default as iceAndEmber } from "@lumines/splash/src/styles/ice-and-ember.less";
+import { default as splashMidnightNeon } from "@lumines/splash/src/styles/midnight-neon.less";
+import { default as sunset } from "@lumines/splash/src/styles/sunset.less";
 import useTimer from "@lumines/core/src/hooks/useTimer";
+import useKey from "@lumines/core/src/hooks/useKey";
 
 const COLORS_SWATCH = 5;
+const SPLASH_STYLES = [iceAndEmber, splashDefault, splashMidnightNeon, sunset];
 
 const Block = (props) => {
   const { classes } = props;
@@ -12,12 +17,18 @@ const Block = (props) => {
 };
 
 const Splash = (props) => {
+  const [styleIndex, setStyleIndex] = useState(() =>
+    Math.floor(Math.random() * SPLASH_STYLES.length),
+  );
+  const splashStyle = SPLASH_STYLES[styleIndex];
+
   const getBlocks = () => {
     const blocks = (
       <>
         {[...Array(8 * 8)].map((item, idx) => {
           const blockIdx = Math.floor(Math.random() * COLORS_SWATCH + 1);
           const blockClass = splashStyle[`block-${blockIdx}`];
+
           return (
             <Block key={idx} classes={`${splashStyle.block} ${blockClass}`} />
           );
@@ -29,9 +40,25 @@ const Splash = (props) => {
 
   const [blocks, setBlocks] = useState(getBlocks);
 
+  useKey((key) => {
+    if (key === "s" || key === "S") {
+      setStyleIndex((prev) => (prev + 1) % SPLASH_STYLES.length);
+    }
+  });
+
+  useEffect(() => {
+    setBlocks(getBlocks());
+  }, [styleIndex]);
+
   useTimer(() => {
     setBlocks(getBlocks());
   }, 2500);
+
+  const startTitle = (
+    <div className={splashStyle.action}>
+      <h4>PRESS SPACE KEY</h4>
+    </div>
+  );
 
   return (
     <div className={splashStyle.root}>
@@ -51,9 +78,7 @@ const Splash = (props) => {
           FUSION
         </h3>
       </div>
-      <div className={splashStyle.action}>
-        <h4>PRESS SPACE KEY</h4>
-      </div>
+      {startTitle}
     </div>
   );
 };
